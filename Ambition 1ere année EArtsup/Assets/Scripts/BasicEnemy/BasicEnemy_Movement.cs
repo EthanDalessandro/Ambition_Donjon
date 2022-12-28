48d9c6a431f40f11e2ca222b5d0d2c168f6384c3
XProKio <90325305+XProKio@users.noreply.github.com>
2022-12-28 21:13:50 +0100

    fix

--- Ambition 1ere année EArtsup/Assets/Scripts/BasicEnemy/BasicEnemy_Movement.cs
+++ Ambition 1ere année EArtsup/Assets/Scripts/BasicEnemy/BasicEnemy_Movement.cs
@@ -5,32 +5,49 @@ using UnityEngine.AI;
 
 public class BasicEnemy_Movement : MonoBehaviour
 {
+    // Variable de stockage de la position d'origine de l'ennemi
     Vector3 originPosition;
+
+    // Composant NavMeshAgent de l'ennemi
     public NavMeshAgent agentPawn;
+
+    // Variable de contrôle du délai de suivi du joueur
     float followSmooth = 0f;
+
+    // Durée du délai de suivi du joueur
     public float followSmoothCD = 0.5f;
 
     public void Start()
     {
         agentPawn = this.GetComponent<NavMeshAgent>();
+        // Enregistre la position d'origine de l'ennemi
         originPosition = transform.position;
 
+        // Définit la destination de l'agent de navmesh sur sa position d'origine
         agentPawn.SetDestination(originPosition);
     }
 
     public void OnTriggerStay(Collider other)
     {
+        // Récupère le script PlayerController attaché à l'objet en collision
         var player = other.GetComponent<PlayerController>();
 
+        // Si l'objet en collision est le joueur
         if (player)
         {
+            // Modifie la distance d'arrêt de l'agent de navmesh
             agentPawn.stoppingDistance = 2f;
 
+            // Incrémente la variable de délai de suivi du joueur
             followSmooth += 1f * Time.deltaTime;
 
+            // Si le délai de suivi du joueur est écoulé
             if (followSmooth >= followSmoothCD)
             {
+                // Remet le délai de suivi du joueur à 0
                 followSmooth = 0f;
+
+                // Met à jour la destination de l'agent de navmesh sur la position du joueur
                 agentPawn.SetDestination(player.transform.position);
             }
         }
@@ -40,12 +57,18 @@ public class BasicEnemy_Movement : MonoBehaviour
     {
         var player = other.GetComponent<PlayerController>();
 
+        // Si l'objet en collision était le joueur
         if (player)
         {
+            // Remet la distance d'arrêt de l'agent de navmesh à 0
             agentPawn.stoppingDistance = 0f;
+
+            // Remet le délai de suivi du joueur à 0
             followSmooth = 0f;
+
+            // Met à jour la destination de l'agent de navmesh sur sa position d'origine
             agentPawn.SetDestination(originPosition);
         }
-    }
 
+    }
 }
--- Ambition 1ere année EArtsup/Assets/Scripts/PlayerController.cs
+++ Ambition 1ere année EArtsup/Assets/Scripts/PlayerController.cs
@@ -3,29 +3,51 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Ce tableau de GameObjects contiendra les audio clips à jouer lors de différentes actions du joueur (attaque, déplacement, etc.)
     public GameObject[] audioArray;
+
+    // Ces vecteurs stockeront les entrées de mouvement et de regard du joueur
     private Vector2 moveAxis;
     private Vector2 lookAxis;
+
+    // Ces variables seront utilisées pour gérer la rotation de la caméra et la vitesse de saut du joueur
     private float xRotation;
     private float ySpeed;
     private bool hasJumped;
+
+    // Ce Character Controller sera utilisé pour gérer la gravité et les collisions du joueur
     public CharacterController characterController;
+
+    // Ces variables définissent la vitesse de déplacement et de rotation du joueur
     public float moveSpeed;
     public float lookSpeed;
+
+    // Ce Transform représente la caméra du joueur
     public Transform camTransform;
+
+    // Ces variables définissent les limites de rotation de la caméra en X (verticalement)
     public float minXRotation;
     public float maxXRotation;
+
+    // Cette variable définit la vitesse de saut du joueur
     public float jumpSpeed;
+
+    // Ces variables définissent la portée de l'attaque du joueur et la force de recul appliquée aux ennemis touchés
     public float attackRange = 1f;
     public float knockBackForce = 12f;
+
+    // Ces variables définissent les dégâts infligés par l'attaque du joueur et les chances de coup critique
     public float damage = 10;
     float damageCalculatedCrit;
     public float critChance = 10;
     public float critDamage = 100f;
+
+    // Ces variables définissent la vie et la vie maximale du joueur
     public float health = 100f;
     public float maxHealth;
-    public Transform direction;
 
+    // Ce Transform représente la direction vers laquelle le joueur regarde (utilisé pour l'attaque)
+    public Transform direction;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -63,31 +85,47 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
+        // Crée un vecteur de direction à partir des entrées de mouvement du joueur (moveAxis)
         Vector3 direction = new Vector3(moveAxis.x, 0f, moveAxis.y);
+
+        // Applique la rotation du joueur au vecteur de direction
         direction = transform.TransformDirection(direction);
+
+        // Multiplie la vitesse de déplacement par le vecteur de direction
         direction *= moveSpeed;
+
+        // Ajoute la gravité à la vitesse verticale du joueur
         ySpeed += Physics.gravity.y * Time.deltaTime;
 
+        // Si le joueur est au sol
         if (characterController.isGrounded)
         {
+            // Réinitialise la vitesse verticale du joueur (afin d'éviter qu'il ne "flotte" au sol)
             ySpeed = -0.5f;
+
+            // Si le joueur a sauté
             if (hasJumped)
             {
+                // Applique la vitesse de saut au joueur
                 ySpeed = jumpSpeed;
             }
         }
 
+        // Ajoute la vitesse verticale au vecteur de direction
         direction.y = ySpeed;
-        //direction.y += Physics.gravity.y * Time.deltaTime;
+        // direction.y += Physics.gravity.y * Time.deltaTime;
+
+        // Utilise le Character Controller pour déplacer le joueur en fonction du vecteur de direction et du temps écoulé depuis la dernière frame
         characterController.Move(direction * Time.deltaTime);
 
+        // Réinitialise la variable hasJumped
         hasJumped = false;
     }
 
     public void RightAttack(InputAction.CallbackContext context)
     {
-        if(context.performed)
-        {    
+        if (context.performed)
+        {
             Debug.DrawRay(direction.position, direction.forward * attackRange, Color.red); //Dessine un Rayon Rouge qui suit les mouvements de la camera
             RaycastHit mouse1_hitObject; //L'objet qui a été toucher par le raycast
             Ray mouse1_Ray = new Ray(direction.position, direction.forward); //j'initie le rayon qui vas apparaitre lors du clique droit
@@ -123,7 +161,7 @@ public class PlayerController : MonoBehaviour
     }
     public void LeftAttack(InputAction.CallbackContext context)
     {
-        if(context.performed)
+        if (context.performed)
         {
             Debug.DrawRay(direction.position, direction.forward * attackRange, Color.blue);
             RaycastHit leftClickHitObject;
